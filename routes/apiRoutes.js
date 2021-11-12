846e86c208bad0db20e43e64d43133b3232a6653
Tammy Gagliano <tracsd@gmail.com>
2021-11-12 13:45:48 -0600

    update to apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,33 @@
-// database 
-const db = require('../models');
-
+//db
+const db = require('../models')
 module.exports = (app) => {
-    
-    // workout routes 
-    app.get('/api/workouts', (req, res) => {
+
+    //get all
+    app.get("/api/workouts", (req, res) => {
         db.Workout.find({}, (err, workouts) => {
-            if(err) {
-                console.log(error);
+            if(err){
+                console.log(err);
             } else {
                 res.json(workouts)
             }
         });
     });
 
-    // add exercise
-    app.put('api/workouts/:workout', ({ params, body}, res) => {
-        db.Workout.findOneAndUpdate({ _id: params.id}, 
-            {push: {exercises:body }}, 
-            { upsert: true, useFindAndModify: false}, 
-                updatedWorkout => {
-                    res.json(updatedWorkout);
-                })
+    //add exercise, set id, push to model, set true
+    app.put("/api/workouts/:workout", ({ params, body }, res) => {
+        db.Workout.findOneAndUpdate({ _id: params.id},
+                                    {$push: {excercises:body }},
+                                    { upsert: true, useFindandModify:false},
+                                    updatedWorkout => {
+                                        res.json(updatedWorkout);
+                                    })
     });
 
-    // create new workout 
-    app.post('/api/workouts', (req, res) => {
+    //create new workout
+    app.post('/api/workouts', (req,res) => {
         db.Workout.create({}).then(newWorkout => {
             res.json(newWorkout);
         });
     });
+
 }
